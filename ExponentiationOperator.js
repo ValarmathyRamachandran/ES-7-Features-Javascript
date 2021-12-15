fe0b66c546cd812ee905389da5c828f11288e319
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-12-15 19:09:58 +0530

    Implemented Es 7 feature exponentiation operator

---
+++ ExponentiationOperator.js
@@ -0,0 +1,8 @@
+//ES 7 provides an alternative way to get a base to the 
+//exponent power by using the exponentiation operator ( **).
+
+var result = 2 ** 2;
+console.log(result); // 4
+
+result = 2 ** 3;
+console.log(result); // 8
